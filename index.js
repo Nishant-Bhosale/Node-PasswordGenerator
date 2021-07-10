1da6b40d3d9022c75aa34fe29ed5728cd2299a23
Nishant <nishantbhosale244@gmail.com>
2021-07-10 12:40:38 +0530

    Finished Projects

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+#!/usr/bin/env node
 const program = require("commander");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
 const createPassword = require("./utils/createPassword");
+const savePassword = require("./utils/savePassword");
 const log = console.log;
+
 program.version("1.0.0").description("Simple Password Generator");
 
 program
 	.option("-l, --length <number>", "Length of Password", "9")
+	.option("-s, --save", "save password to passwords.txt")
 	.option("-nn, --no-numbers", "remove numbers")
 	.option("-ns, --no-symbols", "remove symbols")
 	.parse();
 
-const { length, numbers, symbols } = program.opts();
+const { length, numbers, symbols, save } = program.opts();
 
-log(createPassword(length, numbers, symbols));
+const generatedPassword = createPassword(length, numbers, symbols);
+
+if (save) {
+	savePassword(generatedPassword);
+}
+
+clipboardy.writeSync(generatedPassword);
+
+log(chalk.blue("GeneratedPassword: ") + chalk.bold(generatedPassword));
+log("Password Copied to clipboard");
